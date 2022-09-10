8b0fb406991abcb7ac34acf87b916557854bc358
Pranavvks <pranav.vks@gmail.com>
2022-09-10 14:43:48 +0530

    Completed NFT Metadata Input Form

--- components/ModalSheet/Modal.jsx
+++ components/ModalSheet/Modal.jsx
@@ -8,15 +8,16 @@ import RangeSlider from '../Slider/RangeSlider';
 import { height } from 'styled-system';
 
 
+
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 800,
-    height:900 ,
+    width: 1000,
+    height:1110 ,
     bgcolor: 'background.paper',
-    borderradius: '100%' ,
+    borderradius: '0.5rem' ,
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
@@ -36,25 +37,57 @@ const style = {
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
+            className="rounded"
           >
             <Box sx={style}>
               
               <Typography id="modal-modal-title" variant="h6" component="h2" className='text-3xl font-semibold'>
                 Input NFT Data
               </Typography>
-             
+             <div className='text-gray-400'>
+              The Metadata Fields for your NFT
+             </div>
               <form>
-                <h1 className='mt-4'>Title</h1>
-                <TextField id="demo-helper-text-misaligned-no-helper" sx={{width:"500px"}} />
+              <div className='flex justify-between'>
+                <h1 className='mt-2 ml-2 mr-2 flex-none font-normal '><br /> Title  </h1>
+                <TextField id="demo-helper-text-misaligned-no-helper" className='mt-16 -ml-64  w-96 flex-none' />
+                <h1 className='flex-none -mr-64 mt-10 '>NFT Image</h1>
+                <TextField id="demo-helper-text-misaligned-no-helper" className='mt-16 w-96 -ml-16 flex-none ' />
+                </div>
+                
                 <div className='flex justify-between'>
-                <h1 className='mt-4 flex-none -mr-5'>Keyword hashtags </h1>
-                <TextField id="demo-helper-text-misaligned-no-helper" className='mt-16 -ml-52  w-80' />
-                <h1 className='mt-4 flex-none -mr-48'>Series Name</h1>
-                <TextField id="demo-helper-text-misaligned-no-helper" className='mt-16 w-80 ml-5' />
+                <h1 className='mt-2 mr-8 flex-none '><br /> Keyword Hashtags  </h1>
+                <TextField id="demo-helper-text-misaligned-no-helper" className='mt-16 -ml-96  w-96 flex-none' />
+                <h1 className='flex-none -mr-64 mt-10 '>Series Name</h1>
+                <TextField id="demo-helper-text-misaligned-no-helper" className='mt-16 w-96 -ml-16 flex-none ' />
                 </div>
-                <div className='bg-slate-400 w-[456px] h-[156px] mt-10'>
-                <RangeSlider />
+                <div className='bg-slate-50 w-[966px] h-[206px] mt-10 -ml-5 rounded-lg shadow-lg flex'>
+                 <div className='mt-4 ml-10 text-xl'>
+                 Copies
+                 </div>
+                 <div className='text-red-600  '>
+                 <br />
+                 <br />
+                    <div className=' mt-5 font-medium -ml-16 '>
+                    Warning: the higher number of  <br /> copies you create the less valuable <br /> each  copy is likely to be </div>  
+                    </div> 
+                <RangeSlider arr = {[1,5,10,20,50,100,250,500,1000]} />
+                </div> <div className='bg-slate-50 w-[966px] h-[206px] mt-10 -ml-5 rounded-lg shadow-lg flex'>
+                 <div className='mt-4 ml-10 text-xl'>
+                 Perpetual Royalty
+                 </div>
+                <div className='ml-10 -mt-5'>
+                <RangeSlider arr = {[0,1,2,5,7.5,10,12,15,20]} />
                 </div>
+
+                </div>
+                <div className='mt-5'>
+                  Description
+                </div>
+                <TextField id="demo-helper-text-misaligned-no-helper"  className='w-[900px] mt-5' />
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-56 ml-48 mt-5 rounded-lg">
+  Go to Preview
+</button>
               </form>
              
             </Box>
--- components/Slider/RangeSlider.jsx
+++ components/Slider/RangeSlider.jsx
@@ -6,14 +6,14 @@ import Typography from '@mui/material/Typography';
 import Box from "@mui/material/Box"
 
 
-const RangeSlider = ({classes, label, onChange, value, ...sliderProps}) => {
+const RangeSlider = (props) => {
     
     // const [mouseState, setMouseState] = useState(null);
     // value will be changed only after the mouse event is over.
    
     const [stepIndex, setStepIndex] = useState(0);
-    const values = [1,5,10,20,50,100,250,500,1000];
-    const inputref = useRef(null);
+    const currentvalue = 0 ;
+    
 
     // Here instead of passing the data that is the values as props
     // we can create a reference  between the values and slider input
@@ -26,7 +26,8 @@ const RangeSlider = ({classes, label, onChange, value, ...sliderProps}) => {
     const changeCallback = (e)=>{
         console.log(e.target.value)
         setStepIndex(e.target.value);
-       
+        currentvalue=props.arr[stepIndex]
+        console.log(currentvalue)
     }
 
     //when the mouse event is up , then we call the 
@@ -41,31 +42,28 @@ const RangeSlider = ({classes, label, onChange, value, ...sliderProps}) => {
 
     return (
         
-        <div className="flex flex-col space-y-2 p-2 w-[400px] ">
-        <p>{label}</p>
-        <h3>value:{values[stepIndex]}</h3>
+        <div className="flex flex-col  mt-20 ml-16 w-[400px] ">
+       
         <input type="range" 
         min={0}
         max={8}
-        value={values[stepIndex+1]}
-        width={{maxWidth:"800px"}}
-        step={1}
-       
-        className={`slider ${classes}`}
+        value={props.arr[stepIndex+1]}
+        width={{maxWidth:"900px"}}
+        step={1}     
         id="myRange"
         onInput={changeCallback}
         
         />
-        <ul className="flex justify-between px-[10px]">
-        <li class="flex justify-center relative"><span class="absolute">1</span></li>
-        <li class="flex justify-center relative"><span class="absolute">5</span></li>
-        <li class="flex justify-center relative"><span class="absolute">10</span></li>
-        <li class="flex justify-center relative"><span class="absolute">20</span></li>
-        <li class="flex justify-center relative"><span class="absolute">50</span></li>
-        <li class="flex justify-center relative"><span class="absolute">100</span></li>
-        <li class="flex justify-center relative"><span class="absolute">250</span></li>
-        <li class="flex justify-center relative"><span class="absolute">500</span></li>
-        <li class="flex justify-center relative"><span class="absolute">1K</span></li>
+        <ul className="flex justify-between px-[10px] ">
+        <li class="flex justify-center relative"><span class="absolute">{props.arr[0]}</span></li>
+        <li class="flex justify-center relative"><span class="absolute">{props.arr[1]}</span></li>
+        <li class="flex justify-center relative"><span class="absolute">{props.arr[2]}</span></li>
+        <li class="flex justify-center relative"><span class="absolute">{props.arr[3]}</span></li>
+        <li class="flex justify-center relative"><span class="absolute">{props.arr[4]}</span></li>
+        <li class="flex justify-center relative"><span class="absolute">{props.arr[5]}</span></li>
+        <li class="flex justify-center relative"><span class="absolute">{props.arr[6]}</span></li>
+        <li class="flex justify-center relative"><span class="absolute">{props.arr[7]}</span></li>
+        <li class="flex justify-center relative"><span class="absolute">{props.arr[8]}</span></li>
         </ul>
         
         </div>
